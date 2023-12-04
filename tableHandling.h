c84d67a86ae02f59bdb0e8fb4a2e754235a8cc21
LovIMarK <mark@lovink.ch>
2023-12-04 13:34:30 +0100

    display 2d table in table handling

--- tableHandling.h
+++ tableHandling.h
@@ -2,14 +2,25 @@
 #define TABLEHANDLING
 
 #include <iomanip>
-#include <iostream>
+#include <string>
 
+void displayTitleWithSeparation(const std::string& text, int numberLineSeparator, const std::string& symbolSeparator){
+  static int numberIterartion;
+
+  const int spaceToMiddle= numberLineSeparator/2 + (int(text.size())/2) - 1;
+
+  std::cout << symbolSeparator   << std::endl;
+  std::cout << std::setw(spaceToMiddle) << text << numberIterartion << std::endl;
+  std::cout << symbolSeparator   << std::endl;
+
+  numberIterartion++;
+}
 
 
 template <typename T>
 void display2DTable(T &table, int spaceBetweenDisplay) {
 
-  const unsigned short IMPBRICKEDTABLESIZE = 0;
+  const unsigned short IMBRICKEDTABLESIZE = 0;
 
   std::cout << std::setw(spaceBetweenDisplay) << "";
   for (size_t col = 0; col < table.size(); ++col) {
@@ -17,7 +28,7 @@ void display2DTable(T &table, int spaceBetweenDisplay) {
   }
 
   std::cout << std::endl;
-  for (size_t row = 0; row < table[IMPBRICKEDTABLESIZE].size(); ++row) {
+  for (size_t row = 0; row < table[IMBRICKEDTABLESIZE].size(); ++row) {
     std::cout << std::setw(spaceBetweenDisplay) << row;
     for (size_t col = 0; col < table.size(); ++col) {
       std::cout << std::setw(spaceBetweenDisplay) << table[row][col] ;

0a7c136a0076cfc4ddb5f103ec89a896c1630987
LovIMarK <mark@lovink.ch>
2023-12-04 13:33:47 +0100

    ugly board/ initializeBoard()

--- boardGame.h
+++ boardGame.h
@@ -1,16 +1,31 @@
 #ifndef BOARDGAME
 #define BOARDGAME
 
-
-#include <iostream>
-#include <string>
-
 const unsigned short ROW = 0;
+
+template <typename T, typename AliveCellType,typename DeadCellType>
+T initializeBoard(T& table, AliveCellType aliveCell, DeadCellType deadCell) {
+  const int ALIVECELL=1;
+
+
+  for (size_t row = 0; row < table[ROW].size(); ++row) {
+    for (size_t col = 0; col < table.size(); ++col) {
+      if (table[row][col] == ALIVECELL) {
+        table[row][col] = aliveCell;
+      } else {
+        table[row][col] = deadCell;
+      }
+    }
+  }
+  return table;
+}
 
 
 template <typename T ,typename AliveCellType>
 int countLiveNeighbors(T& actualBoard, std::size_t row, std::size_t col,AliveCellType aliveCell){
   int aliveNeighbors = 0;
+
+
   for (int rowNextToCell = -1; rowNextToCell <= 1; ++rowNextToCell) {
     for (int colNextToCell = -1; colNextToCell <= 1; ++colNextToCell) {
       if ( row + size_t(rowNextToCell) < actualBoard.size()  && col + size_t(colNextToCell) < actualBoard[row].size()) {
@@ -25,10 +40,12 @@ int countLiveNeighbors(T& actualBoard, std::size_t row, std::size_t col,AliveCel
 
 template <typename T ,typename AliveCellType,typename DeadCellType>
 void updateBoard(T& actualBoard, AliveCellType aliveCell, DeadCellType deadCell) {
+
   T newBoard = actualBoard;
+
   for (size_t row = 0; row < actualBoard[ROW].size(); ++row) {
     for (size_t col = 0; col < actualBoard.size(); ++col) {
-      int liveNeighbors = countLiveNeighbors(actualBoard, row, col,aliveCell) - ((actualBoard[row][col] == aliveCell) ? 1:0);
+      int liveNeighbors = countLiveNeighbors(actualBoard, row, col, aliveCell) - ((actualBoard[row][col] == aliveCell) ? 1 : 0);
 
       if (actualBoard[row][col] == aliveCell) {
         newBoard[row][col] = (liveNeighbors < 2 || liveNeighbors > 3) ? deadCell : aliveCell;
@@ -40,10 +57,6 @@ void updateBoard(T& actualBoard, AliveCellType aliveCell, DeadCellType deadCell)
 
   actualBoard = newBoard;
 }
-
-
-
-
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <array>
 #include <thread>
 #include "boardGame.h"
@@ -26,63 +25,45 @@ const char DEADCELL          = '.';
 using Board = array<array<char, TABLECOL>, TABLEROW>;
 
 
-
-
 const int    NUMMBERLINESEPARATOR        = SPACINGFORCASE * (TABLEROW+1);
 const string TABLEGENERATIONBORDEUR(size_t(NUMMBERLINESEPARATOR), '-');
 const string TITLEGENERARTION       = "GENRATION NO ";
 
 
 
-void displayTitleWithSeparation(const string& text, int numberLineSeparator, const string& symbolSeparator){
-  static int numberIterartion;
-
-  const int spaceToMiddle= numberLineSeparator/2 + (int(text.size())/2) - 1;
-
-  cout << symbolSeparator   << endl;
-  cout << setw(spaceToMiddle) << text << numberIterartion << endl;
-  cout << symbolSeparator   << endl;
-
-  numberIterartion++;
-}
-
-
-
-
-
 
 
 int main() {
-  // J'a enlevé la fonction pour affecter le tableau
+  // problème si on change la taille de TABLEROW / TABLECOL il faut aussi modifier tout ce tableau avec des 0 et 1
   Board actualBoard = {{
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //0
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //1
-      {DEADCELL ,DEADCELL ,DEADCELL ,ALIVECELL,ALIVECELL,ALIVECELL,DEADCELL, DEADCELL, DEADCELL}, //2
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,ALIVECELL,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //3
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,ALIVECELL,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //4
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,ALIVECELL,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //5
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //6
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //7
-      {DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL ,DEADCELL, DEADCELL, DEADCELL}, //8
-  }};
-  // J'ai enlevé la condition avec le break pour éviter le break
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                           {0, 0, 0, 0, 1, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 1, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 1, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0}
+                       }};
+
+  initializeBoard(actualBoard, ALIVECELL, DEADCELL);
   Board oldBoard = {{{}}};
-  // Ajout de la condition dans la for
+
   for (int i = 0; i < MAXGAMEGENERATION and actualBoard != oldBoard; ++i) {
-    // Est-ce possible d'avoir plus de clareté dans les noms de fonctions ?
-    displayTitleWithSeparation(TITLEGENERARTION,NUMMBERLINESEPARATOR,TABLEGENERATIONBORDEUR);
+    displayTitleWithSeparation(TITLEGENERARTION, NUMMBERLINESEPARATOR, TABLEGENERATIONBORDEUR);
     display2DTable(actualBoard, SPACINGFORCASE);
     oldBoard = actualBoard;
-    updateBoard(actualBoard,ALIVECELL,DEADCELL);
+    updateBoard(actualBoard, ALIVECELL, DEADCELL);
 
-    // Changement afin d'éviter un fichier
     this_thread::sleep_for(chrono::milliseconds(SLEEPTIMEMS));
-    // Ajout de la condition après le 'and ...'
+
     if (i < MAXGAMEGENERATION - 1 and actualBoard != oldBoard)
       CLEARTERMINAL
 
   }
-  // Ajout de l'attente que l'utilisateur appuisse sur enter pour terminer le programme
+
+  //any key au lieu de enter
   cout << endl << "Press any key to finish ...";
   cin.get();
   return EXIT_SUCCESS;
